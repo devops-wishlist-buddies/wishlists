44baa321845fa8d421da90c748b6a2a160138f72
Natalia Zubkova <nataliezubkov@gmail.com>
2021-10-06 16:09:20 -0400

    Create a skeleton flask app

---
+++ service/routes.py
@@ -0,0 +1,60 @@
+# Copyright 2016, 2019 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Wishlists Service
+
+Paths:
+------
+TBD
+"""
+
+import os
+import sys
+import logging
+from flask import Flask, jsonify, request, url_for, make_response, abort
+from . import status  # HTTP Status Codes
+from werkzeug.exceptions import NotFound
+
+# For this example we'll use SQLAlchemy, a popular ORM that supports a
+# variety of backends including SQLite, MySQL, and PostgreSQL
+from flask_sqlalchemy import SQLAlchemy
+
+# Import Flask application
+from . import app
+
+######################################################################
+# GET INDEX
+######################################################################
+@app.route("/")
+def index():
+    """ Root URL response """
+    return (
+        jsonify(
+            name="Wishlists REST API Service",
+            version="1.0",
+            paths=url_for("wishlists_index", _external=True),
+        ),
+        status.HTTP_200_OK,
+    )
+
+## Define functional routes below
+@app.route("/wishlists", methods=["GET"])
+def wishlists_index():
+  return (
+        jsonify(
+            {"message": "Nothing here yet!" }
+        ),
+        status.HTTP_200_OK,
+    )
