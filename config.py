f6a2eac44e7e98fcb0ff2c02eac6b14171fc606c
yx2512 <yx2512@nyu.edu>
2021-12-06 03:33:41 -0500

    update manifest.yml

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DATABASE_URI = os.getenv(
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
-    DATABASE_URI = vcap['user-provided'][1]['credentials']['url']
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
