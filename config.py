09a8c8ff37c978c35e9a75a0718953cdec6961a2
Yu Xia <yx2512@nyu.edu>
2021-12-06 07:16:41 +0000

    refactor add to cart

--- config.py
+++ config.py
@@ -6,14 +6,15 @@ import json
 
 # Get configuration from environment
 # DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///../development.db")
-DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
-)
 
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
     DATABASE_URI = vcap['user-provided'][1]['credentials']['url']
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
+)
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
